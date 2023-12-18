a9fc42651251b4a141761599d31e90427c3eb6ec
rsdiaz <robertoserrano83@gmail.com>
2023-12-18 01:52:26 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,25 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-          // Example: Disable injecting a basic `base.css` import on every page.
-          // Useful if you need to define and/or import your own custom `base.css`.
-          applyBaseStyles: false,
-        }),
-      ],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+  ],
 });
