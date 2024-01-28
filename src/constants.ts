194e4af29f1bb1062a22307c6eeb90d3f5b6d9c7
rsdiaz <robertoserrano83@gmail.com>
2024-01-29 00:34:04 +0100

    timeline data

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,30 @@ export const GITHUB_PROFILE_URL = 'https://github.com/rsdiaz'
 export const TWITTER_PROFILE_URL = 'https://twitter.com/Rovbeat'
 export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/roberto-serrano-diaz-grande'
 export const NPM_PROFILE_URL = 'https://www.npmjs.com/~robfree'
+
+export const timelineData = [
+    {
+        title: 'Full Stack Web Developer 2012 - ¿? 😉',
+        description: ''
+    },
+    {
+        title: 'Administrados de Sistemas / Junior Web Developer Junio 2014 - 2016 & The Art Group',
+        description: ''
+    },
+    {
+        title: 'Web Developer Junio 2016 - 2017 & Bukimedia',
+        description: ''
+    },
+    {
+        title: 'Administrados de Sistemas / Full Stack Web Developer Junio 2017 - 2021 & TICNOVA GROUP',
+        description: ''
+    },
+    {
+        title: 'Full Stack Web Developer Junio 2017 - 2023 & Grup Oliva Motor',
+        description: ''
+    },
+    {
+        title: 'Tech Lead Junio 2023 - presente & Grup Oliva Motor',
+        description: ''
+    },
+].reverse()
