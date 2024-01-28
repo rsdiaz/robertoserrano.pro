275b2a4ab6addb06800ce5f2cb077afef629ecb1
rsdiaz <robertoserrano83@gmail.com>
2024-01-28 23:47:51 +0100

    add constants for the social profiles

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const GITHUB_PROFILE_URL = 'https://github.com/rsdiaz'
+export const TWITTER_PROFILE_URL = 'https://twitter.com/Rovbeat'
+export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/roberto-serrano-diaz-grande'
+export const NPM_PROFILE_URL = 'https://www.npmjs.com/~robfree'
