c85d7d4208db37d683ee249873ad2fdbef7faa15
rsdiaz <robertoserrano83@gmail.com>
2024-01-30 23:15:27 +0100

    update

--- src/constants.ts
+++ src/constants.ts
@@ -2,30 +2,43 @@ export const GITHUB_PROFILE_URL = 'https://github.com/rsdiaz'
 export const TWITTER_PROFILE_URL = 'https://twitter.com/Rovbeat'
 export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/roberto-serrano-diaz-grande'
 export const NPM_PROFILE_URL = 'https://www.npmjs.com/~robfree'
+export const STATIC_OG_IMAGE_URL = 'https://res.cloudinary.com/rserrano/image/upload/v1706652797/Screenshot_2024-01-30_at_23-09-52_Sobre_mi_-_Roberto_Serrano_desarrollador_de_software_en_Tarragona_ooz8y6.png'
 
 export const timelineData = [
     {
-        title: 'Full Stack Web Developer 2012 - ¿? 😉',
-        description: ''
+        position: 'Geek',
+        site: 'localhost',
+        date:"1989 - presente",
+        description: 'AMSTRAMD CPC + un manual de BASIC - Aquí comenzó todo...'
     },
     {
-        title: 'Administrados de Sistemas / Junior Web Developer Junio 2014 - 2016 & The Art Group',
-        description: ''
+        position: 'SYSADMIN / Junior Web Developer',
+        site: 'Grupo Arte',
+        date:"2014 - 2016",
+        description: 'Redes locales, GNU/Linux, Windows AD, MySQL, PHP, Joomla, WordPress,'
     },
     {
-        title: 'Web Developer Junio 2016 - 2017 & Bukimedia',
-        description: ''
+        position: 'Web Developer',
+        site: 'Bukimedia',
+        date:"2016 - 2017",
+        description: 'C#, .NET, PHP, Prestashop, WordPress'
     },
     {
-        title: 'Administrados de Sistemas / Full Stack Web Developer Junio 2017 - 2021 & TICNOVA GROUP',
-        description: ''
+        position: 'SYSADMIN / Web Full Stack',
+        site: 'TICNOVA GROUP',
+        date:"2017 - 2021",
+        description: 'Proxmox, WordPress, NodeJS, Express, React, MongoDB'
     },
     {
-        title: 'Full Stack Web Developer Junio 2017 - 2023 & Grup Oliva Motor',
-        description: ''
+        position: 'Web Developer Full Stack',
+        site: 'Grup Oliva Motor',
+        date:"2017 - 2023",
+        description: 'WordPress, NodeJS, Express, React, NextJS, MongoDB, SQL'
     },
     {
-        title: 'Tech Lead Junio 2023 - presente & Grup Oliva Motor',
-        description: ''
+        position: 'Tech Lead',
+        site: 'Grup Oliva Motor',
+        date:"2023 - presente",
+        description: 'NodeJS, Express, React, NextJS, MongoDB, SQL, WordPress, Azure'
     },
-].reverse()
+]
