33b1413c5e06f99ff0869f9c8e9e1520e3e9c57e
rsdiaz <robertoserrano83@gmail.com>
2024-02-04 21:45:39 +0100

    add hhtps to site url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = "http://robertoserrano.pro"
+export const SITE_URL = "https://robertoserrano.pro"
 export const GITHUB_PROFILE_URL = 'https://github.com/rsdiaz'
 export const TWITTER_PROFILE_URL = 'https://twitter.com/Rovbeat'
 export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/roberto-serrano-diaz-grande'
